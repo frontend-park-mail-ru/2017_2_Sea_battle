2b2b0015f37bd98f8b33ca5131fd41d94709767d
Евгений <johnbuevich@yahoo.com>
2017-12-15 17:10:13 +0300

    fixed blue colors

--- Data/Game/GameLogic.js
+++ Data/Game/GameLogic.js
@@ -186,8 +186,7 @@ export default class GameLogic
 
         for (let i = 0; i < data.destroyedShip.cellsAroundShip.length; i++) {
             field = document.getElementById(data.destroyedShip.cellsAroundShip[i].rowPos + flag + (data.destroyedShip.cellsAroundShip[i].colPos));
-            field.classList.remove("Fire");
-            field.classList.add("blocked");
+            field.classList.add("Fire");
         }
 
     }
