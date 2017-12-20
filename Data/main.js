105e6330a6bd1be37226598805ca8f74e87fdfc9
YAXo-O <aaazzzx@mail.ru>
2017-12-21 00:43:54 +0300

    Leaderboard fixed

--- Data/main.js
+++ Data/main.js
@@ -1,6 +1,9 @@
 "use strict";
+// JS
 import MenuManager from "./Modules/MenuManager.js";
 import EventBus from "./Modules/EventBus.js";
+import LoadingScreen from "./Modules/Blocks/LoadingScreen/LoadingScreen.js";
+// CSS
 import "./CSS/Styles.css"
 import "./Game/CSS/GameStyle.css"
 import "./Game/CSS/LoaderStyle.css"
@@ -12,11 +15,17 @@ import "./Modules/Views/BaseMenuView/BaseMenuView.css"
 import "./Modules/Views/BaseView/BaseView.css"
 import "./Modules/Views/LeaderboardView/LeaderboardView.css"
 import "./Modules/Blocks/MessageBox/MessageBox.css";
+import "./Modules/Blocks/LoadingScreen/LoadingScreen.css";
 
 const eventBus = new EventBus();
 const menuManager = new MenuManager();
 eventBus.subscribe(menuManager);
 menuManager.go();
+
+
+//let screen = new LoadingScreen("Some loading text");
+//screen.show();
+
 
 
 window.onpopstate = menuManager.go.bind(menuManager);
