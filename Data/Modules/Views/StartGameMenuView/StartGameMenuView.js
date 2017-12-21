e719262ef84253abf66d0df03b3c78223dd026a3
Евгений <johnbuevich@yahoo.com>
2017-12-21 17:45:51 +0300

    fix pug-loader

--- Data/Modules/Views/StartGameMenuView/StartGameMenuView.js
+++ Data/Modules/Views/StartGameMenuView/StartGameMenuView.js
@@ -1,20 +1,13 @@
 "use strict";
 
 import BaseView from "../BaseView/BaseView.js";
-<<<<<<< HEAD
-=======
-let generateStartGameMenuView = require("./StartGameMenuView.pug");
->>>>>>> 603d04f86a1a6818d43151c9e44048c2a64744ac
 
 const startGameMenuView = new BaseView(document.body, generateStartGameMenuView,
     {
         title: "Start Game",
         menus:
             [
-<<<<<<< HEAD
                 {name: "Quick play", id: "/startGame/multiplayer"},
-=======
->>>>>>> 603d04f86a1a6818d43151c9e44048c2a64744ac
                 {name: "Log-in", id: "/startGame/login"},
                 {name: "Register", id: "/startGame/register"},
             ]
